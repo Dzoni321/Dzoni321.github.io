14bb6c52f924d71ea3b0153c84c1065cdc1c84da
Dzoni321 <33811929+Dzoni321@users.noreply.github.com>
2018-02-12 17:13:04 +0100

    a

--- main.js
+++ main.js
@@ -176,10 +176,10 @@ function openPage(pageName,elmnt,color) {
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
-    }
+    //tablinks = document.getElementsByClassName("tablink");
+    //for (i = 0; i < tablinks.length; i++) {
+      //  tablinks[i].style.backgroundColor = "";
+    //}
     document.getElementById(pageName).style.display = "block";
    // elmnt.style.backgroundColor = color;
 
