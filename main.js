2f91ba57b3cb7612af433897a7d1df533b6ede78
Dzoni321 <33811929+Dzoni321@users.noreply.github.com>
2018-02-12 17:04:51 +0100

    a

--- main.js
+++ main.js
@@ -181,7 +181,7 @@ function openPage(pageName,elmnt,color) {
         tablinks[i].style.backgroundColor = "";
     }
     document.getElementById(pageName).style.display = "block";
-    elmnt.style.backgroundColor = color;
+   // elmnt.style.backgroundColor = color;
 
 }
 
